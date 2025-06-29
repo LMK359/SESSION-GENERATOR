fbfa0490bcc928c665ed9c4b8d55a93d767e393a
LMK359 <lmkboy422@gmail.com>
2025-06-29 20:39:48 +0200

    Update qr.js

--- qr.js
+++ qr.js
@@ -22,9 +22,9 @@ const MESSAGE = process.env.MESSAGE ||  `
 └────────────⭓
 ┌───⭓
 ❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/WASI-MD_
-❒ *Author:* _wa.me/923192173398_
-❒ *YT:* _https://youtube.com/@wasitech10_
+❒ *GitHub:* __https://github.com/LMK359_
+❒ *Author:* _wa.me/27727398235_
+❒ *YT:* _https://youtube.com/@lmkagent_
 └────────────⭓
 `
 
